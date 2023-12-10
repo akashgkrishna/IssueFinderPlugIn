2b274a2a46fbe98e0ebc584edbd01c332462bf18
akashkrishna7 <akashkg@testvagrant.com>
2023-12-10 16:04:51 +0530

    Added Options in message pop up

--- src/main/java/com/example/actions/SampleActionClass.java
+++ src/main/java/com/example/actions/SampleActionClass.java
@@ -8,6 +8,8 @@ import org.jetbrains.annotations.NotNull;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,9 +21,10 @@ public class SampleActionClass extends AnAction {
     }
 
     private void findAndShowJiraIDs(String filePath) {
+        List<String> foundIDs = new ArrayList<>();
+
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
-            int lineNumber = 1;
             Pattern commentPattern = Pattern.compile("//.*|/\\*(.|\\n)*?\\*/");
             Pattern jiraIDPattern = Pattern.compile("\\b[A-Z]{2,}-\\d+\\b");
 
@@ -33,14 +36,32 @@ public class SampleActionClass extends AnAction {
                     Matcher jiraIDMatcher = jiraIDPattern.matcher(comment);
                     while (jiraIDMatcher.find()) {
                         String jiraID = jiraIDMatcher.group();
-                        Messages.showInfoMessage("Found Jira ID in comment: " + jiraID, "Jira ID");
-                        // You can perform other actions with the Jira ID here
+                        foundIDs.add(jiraID);
                     }
                 }
-                lineNumber++;
             }
         } catch (IOException ex) {
+            // Replace printStackTrace with more robust logging
             ex.printStackTrace();
+            return;
+        }
+
+        // Display all found IDs together with a dialog including a button
+        if (!foundIDs.isEmpty()) {
+            StringBuilder message = new StringBuilder();
+            for (String id : foundIDs) {
+                message.append("Found Jira ID in comment: ").append(id).append("\n");
+            }
+
+            int choice = Messages.showOkCancelDialog(message.toString() + "\nClick OK for an additional action.",
+                    "Jira IDs", "OK", "Cancel", Messages.getInformationIcon());
+
+            if (choice == Messages.OK) {
+                // Perform additional action on OK button click
+                Messages.showInfoMessage("Hello Clicked!", "Hello");
+            }
+        } else {
+            Messages.showInfoMessage("No Jira IDs found.", "Jira IDs");
         }
     }
 }
